86076c6644f49f6d932947873e750ead4e0e752c
Ruben Sanchez <ruben.sanchezi2007@gmail.com>
2025-05-09 18:23:34 +0200

    initial commit

---
+++ src/main/java/com/capitole/inditex/CapitoleInditexServiceApplication.java
@@ -0,0 +1,13 @@
+package com.capitole.inditex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitoleInditexServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapitoleInditexServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capitole/inditex/CapitoleInditexServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capitole.inditex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapitoleInditexServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
